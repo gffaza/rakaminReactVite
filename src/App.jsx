af073a4563501cb31512bba77b0c186b2417f667
Ghilman Faza <72027610+gffaza@users.noreply.github.com>
2024-08-28 14:05:19 +0700

    adding navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import Navbar from './components/Navbar.jsx';
+
 
 export default function App() {
   return (
     <>
-      {/* <img src="https://logos-world.net/wp-content/uploads/2023/04/Hello-Kitty-Logo.png" alt="Logo" /> */}
-      <h1 className="text-3xl font-bold underline">
-        Hello Kitty!
-      </h1>
+
+      <div className='bg-gray-100 min-h-screen'>
+        <Navbar />
+      </div>
+
     </>
   )
 }
